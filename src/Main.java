453bd9a97eeb82a386284efae8ba7ac9479b39dd
Thao Chi <tranluongthaochi2003@gmail.com>
2023-09-15 08:20:47 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Thao Chi
+ */
+public class Main {
+    public static void main(String[] args) {
+        //loop until user want to exit
+        while (true) {
+            int choice = Manager.menu();
+            switch (choice) {
+                case 1:
+                    String binary = Validate.checkInputBinary();
+                    Manager.convertFromBinary(binary);
+                    break;
+                case 2:
+                    String decimal = Validate.checkInputDecimal();
+                    Manager.convertFromDecimal(decimal);
+                    break;
+                case 3:
+                    String hexa = Validate.checkInputHexaDecimal();
+                    Manager.convertFromHexa(hexa);
+                    break;
+                case 4:
+                    return;
+            }
+        }
+    }
+}
